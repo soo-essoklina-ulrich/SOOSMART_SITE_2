de35c4f984fd6836c16895a9efd2303321435194
soo-essoklina-ulrich <sessoklinaulrich@gmail.com>
2025-02-22 22:05:46 +0000

    use pnpm and add logo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,7 @@ export default defineNuxtConfig({
         }
       ],
       link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        { rel: 'icon', type: 'image/x-icon', href: '/identity.png' }
       ]
     }
   },
